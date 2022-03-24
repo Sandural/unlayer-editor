f898dae57315b199c3c1a46c0d98b272639a6240
v_mningchen <v_mningchen@tencent.com>
2022-03-24 18:41:09 +0800

    init

---
+++ src/components/loadScript.js
@@ -0,0 +1,47 @@
+// const scriptUrl = '//editor.unlayer.com/embed.js?2';
+const scriptUrl = '//email-editor-1303099125.cos.ap-guangzhou.myqcloud.com/embed.js';
+const callbacks = [];
+let loaded = false;
+
+const isScriptInjected = () => {
+  const scripts = document.querySelectorAll('script');
+  let injected = false;
+
+  scripts.forEach((script) => {
+    if (script.src.includes(scriptUrl)) {
+      injected = true;
+    }
+  });
+
+  return injected;
+};
+
+const addCallback = (callback) => {
+  callbacks.push(callback);
+};
+
+const runCallbacks = () => {
+  if (loaded) {
+    let callback;
+
+    while ((callback = callbacks.shift())) {
+      callback();
+    }
+  }
+};
+
+export const loadScript = (callback) => {
+  addCallback(callback);
+
+  if (!isScriptInjected()) {
+    const embedScript = document.createElement('script');
+    embedScript.setAttribute('src', scriptUrl);
+    embedScript.onload = () => {
+      loaded = true;
+      runCallbacks();
+    };
+    document.head.appendChild(embedScript);
+  } else {
+    runCallbacks();
+  }
+};
